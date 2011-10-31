5793a051d640e8a82562bbdc8f33b302f5fd152e
daifu ye <daifu.ye@gmail.com>
2011-10-31 16:54:53 -0700

    Fixed event array bug
    In the eventful library, it pops up the array and it does not need to
    use array_shift in the view.

--- web/application/views/events_la.php
+++ web/application/views/events_la.php
@@ -114,7 +114,7 @@
             <?php
                 foreach($la_events as $e) {
                     $event = (object)$e;
-                    $title = array_shift($event->title);
+                    $title = $event->title;
 					echo "<h3><a href='#'>";
 					echo $title;
 					echo "</a></h3>";
@@ -123,25 +123,25 @@
 
                     // start time
                     echo "<tr><th>Starts</th><th>";
-                    $start = array_shift($event->start_time);
+                    $start = $event->start_time;
                     echo date("F j, Y, g:i a", strtotime($start));
                     echo "</th></tr>";
 
                     // end time
                     echo "<tr><th>Ends</th><th>";
-                    $stop = array_shift($event->stop_time);
+                    $stop = $event->stop_time;
                     echo date("F j, Y, g:i a", strtotime($stop));
                     echo "</th></tr>";
                     
                     // venue
                     echo "<tr><th>Venue</th><th>";
-                    $venue = array_shift($event->venue);
+                    $venue = $event->venue_name;
                     echo $venue;
                     echo "</th></tr>";
                     
                     // Description
 					echo "<tr><th>Description</th><th>";
-                    $desc = array_shift($event->description);
+                    $desc = $event->description;
 					echo nl2br($desc);
                     echo "</th></tr>";
                     echo "</tbody></table>";
