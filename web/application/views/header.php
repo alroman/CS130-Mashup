a8a7a72213759bbc7874c54100ac7fc9e188a86f
Alfonso Roman <alroman@ucla.edu>
2011-11-09 08:58:10 -0800

    mooved bootstrap.css into css folder

--- web/application/views/header.php
+++ web/application/views/header.php
@@ -4,7 +4,7 @@
     
     <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
 
-    <link rel="stylesheet" href="<?php echo base_url() ?>bootstrap.min.css"></link>
+    <link rel="stylesheet" href="<?php echo base_url() ?>css/bootstrap.min.css"></link>
     <link rel="stylesheet" href="<?php echo base_url() ?>css/styles.css"></link>
     <script src="<?php echo base_url() ?>js/jquery-1.6.2.min.js" ></script>
     <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
@@ -71,6 +71,7 @@
                 }
 
                 marker.setIcon(icons['train-selected']);  
+                
                 if (currentPlace) {  
                     currentPlace.setIcon(icons['train']);  
                     info.animate(  
@@ -82,7 +83,7 @@
                                 currentPlace = null;  
                             }  
                         }}  
-                );  
+                    );  
                 } else {  
                     slideIn(marker);  
                 }  
@@ -93,6 +94,7 @@
 
     });
     </script>
+    
     <title><?php echo $page_title ?></title>
 
 </head>
