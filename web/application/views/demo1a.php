3f7476d88d696decf561fcc94133d5e35af153f2
Alfonso Roman <alroman@ucla.edu>
2011-11-09 08:49:14 -0800

    separating header/body/footer into separate views.. this way we're all synchronized.  also moving maps javascript into javascript folder

--- web/application/views/demo1a.php
+++ web/application/views/demo1a.php
@@ -1,18 +1,25 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    
-    <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
-    <link rel="stylesheet" href="http://twitter.github.com/bootstrap/1.3.0/bootstrap.min.css"></link>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js"></script>  
-    <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>  
-        
-	<meta charset="utf-8">
-	<title>test</title>
 
-</head>
-<body>
-    test
-    
-</body>
-</html>
+<div class="topbar">
+  <div class="fill">
+    <div class="container">
+      <a class="brand" href="<?php echo base_url() ?>index.php/demo1">Entertainment+</a>
+
+      <ul class="nav">
+        <li class="active"><a href="<?php echo base_url() ?>index.php/demo1">Home</a></li>
+        <li><a href="<?php echo base_url() ?>index.php/unit_test">Unit Test</a></li>
+      </ul>
+      <form class="pull-right" action="<?php echo base_url() ?>index.php/demo1/" method="post">
+        <input name="city_search" placeholder="Search" type="text">
+        <?php echo form_submit('','Submit'); ?>
+      </form>
+    </div>
+  </div>
+</div>
+
+<div class="map">
+    <div id="map_canvas" style="width: 100%; height: 100%"></div>
+    <div id='placeDetails'>
+        <div id="event_title"></div>
+        <div id="event_desc"></div>  
+    </div>
+</div>
---
+++ web/application/views/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
+
+    <link rel="stylesheet" href="<?php echo base_url() ?>bootstrap.min.css"></link>
+    <link rel="stylesheet" href="<?php echo base_url() ?>css/styles.css"></link>
+    <script src="<?php echo base_url() ?>js/jquery-1.6.2.min.js" ></script>
+    <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
+    <script type="text/javascript" src="<?php echo base_url() ?>/js/e_maps.js"></script>
+    <meta charset="utf-8">
+    
+    <title><?php echo $page_title ?></title>
+
+</head>
+<body>
