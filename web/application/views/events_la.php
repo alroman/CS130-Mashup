b61475fbad98312d1eb9ab953fd61cfd66786a4b
Alfonso Roman <alroman@ucla.edu>
2011-11-20 00:04:44 -0800

    removed accordion for easier viewing.  guys, seriously.. stop using the evens_la controller.. make a new one!!

--- web/application/views/events_la.php
+++ web/application/views/events_la.php
@@ -122,7 +122,7 @@
         <div class="row">
           <div class="span10">
             <h2>Events:</h2>
-            <div id="accordion">
+            <div>
             <?php
                 foreach($la_events as $e) {
                     $event = (object)$e;
@@ -192,85 +192,7 @@
 
     </div> <!-- /container -->
 
-<!-- Javascript for event calendar -->
-<script type="text/javascript" src="http://localhost/CS130-Mashup/web/js/fullcalendar.min.js"></script>
-<script type="text/javascript" src="http://localhost/CS130-Mashup/web/js/jquery.calendarPicker.js"></script>
-<script type="text/javascript" src="http://localhost/CS130-Mashup/web/js/calendar.js"></script>
 
-<script>
-$(document).ready(function() {
-  ;var e = <?php echo $events_cal; ?>;
-  // If the e is buggy, try the complete string instead of the object
-  // var e = '[{"title":"Forbidden City NightClub","start_time":"2011-11-01 18:30:00","stop_time":""},{"title":"RELATIONS - All Things House","start_time":"2011-11-03 00:00:00","stop_time":""},{"title":"CLUB GABAH","start_time":"2011-11-05 22:30:00","stop_time":"2011-11-06 01:00:00"},{"title":"Indigo Lounge","start_time":"2011-10-31 20:00:00","stop_time":"2011-11-01 00:00:00"},{"title":"Hollywood Blonde","start_time":"2011-11-05 21:00:00","stop_time":""},{"title":"Writer Prima","start_time":"2011-11-01 19:00:00","stop_time":""},{"title":"Julia Price","start_time":"2011-11-01 22:00:00","stop_time":""},{"title":"dj tory tee","start_time":"2011-11-02 21:00:00","stop_time":""},{"title":"Marv Robinson","start_time":"2011-11-04 20:15:00","stop_time":""},{"title":"Local Culture","start_time":"2011-11-05 20:00:00","stop_time":""},{"title":"LA Sucks","start_time":"2011-10-31 00:00:00","stop_time":""},{"title":"ACTING CLASSES from a star of the film ALIVE - Los Angeles, CA","start_time":"2011-10-31 00:00:00","stop_time":""},{"title":"ACTING STUDIO: Acting Classes from a star of the film Alive - Los Angeles, CA","start_time":"2011-10-31 00:00:00","stop_time":""},{"title":"A MONTH OF FREE ACTING CLASSES A star of the film ALIVE - Los Angeles, CA","start_time":"2011-10-31 00:00:00","stop_time":""},{"title":"LEARN TO WORK IN FILM AND TELEVISION AS A PROFESSIONAL ACTOR Los Angeles, CA","start_time":"2011-10-31 00:00:00","stop_time":""},{"title":"CASABLANCA (1942) & THE MALTESE FALCON (1941)","start_time":"2011-11-04 19:30:00","stop_time":"2011-11-04 23:25:00"},{"title":"The 3rd Annual Los Angeles Transgender Film Festival: Truth to Power","start_time":"2011-11-06 15:00:00","stop_time":"2011-11-06 16:00:00"},{"title":"New Media Film Festival","start_time":"2011-11-04 00:00:00","stop_time":"2011-11-05 00:00:00"},{"title":"The 3rd Annual Los Angeles Transgender Film Festival - Orchids: My Intersex Adventure","start_time":"2011-11-06 18:00:00","stop_time":"2011-11-06 20:00:00"},{"title":"MET Siegfried LIVE","start_time":"2011-11-05 00:00:00","stop_time":""}]';
-
-    var event_cal = {'events':null, 'eventList':new Array(), 'eventDateList':new Array()};
-    var date = new Date();
-    var d = date.getDate();
-    var m = date.getMonth();
-    var y = date.getFullYear();
-
-    // Initialize the event objects the  event_cal object, which is used lateron
-    event_cal.initEvents = function(e) {
-      event_cal.events = e;
-    }
-
-    event_cal.parse_date = function(string) {
-      var parts = String(string).split(/[- :]/);
-      var date = new Date(parts[0], (parts[1] - 1), parts[2], parts[3], parts[4], parts[5]);
-
-      return date;
-    }
-
-    // Get the eventList and eventDateList populated with the data
-    event_cal.constructEventsList = function() {
-      $.each(event_cal.events, function(i, v){
-        var tmp = {title: v.title,
-              start: v.start_time,
-              end: v.stop_time,
-              allDay: false};
-        event_cal.eventList.push(tmp);
-        // console.log(v.start_time);
-        var date = event_cal.parse_date(v.start_time);
-        // console.log(date.getMonth());
-        event_cal.eventDateList.push(date.toDateString());
-      })
-    };
-
-    event_cal.initEvents(e);
-    event_cal.constructEventsList();
-    // console.log(event_cal.eventDateList);
-
-    var fullCal = $('#calendar').fullCalendar({
-      header: false,
-        height: 300,
-        contentHeight: 200,
-        editable: false,
-        defaultView: 'agendaDay',
-        slotMinutes: 60,
-        allDaySlot: false,
-        firstHour: 0,
-        events: event_cal.eventList
-    });
-
-   var calendarPickr = $("#dest").calendarPicker({
-      monthNames:["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
-       dayNames: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
-       //useWheel:true,
-       //callbackDelay:500,
-       enableYears: false,
-       //months:3,
-       //days:4,
-       //showDayArrows:false,
-       eventDates: event_cal.eventDateList,
-       callback:function(cal) {
-        fullCal.fullCalendar('gotoDate', cal.currentDate);
-       }
-     });
-
-     //Added color for odd lines of the calendar
-     $("table.fc-agenda-slots tr:even").addClass('tr_odd');
-});
-</script>
 
 </body>
 </html>
